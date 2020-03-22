f64b5f53be9b350ab57c3ddc62f3e41171f3a771
Elya <elfucio@gmail.com>
2020-03-22 15:07:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let brackets = 0;
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] == '(') {  // проверяем открытую скобку
+            brackets++; // увеличиваем счет скобок на 1
+        }
+
+        if (expr[i] == ')') {  // проверяем закрытую скобку
+            brackets--; // уменьшаем кол-во скобок на 1
+        }
+    }
+
+    if (brackets !== 0 ) { // если кол-во скобок больше 0, то где-то не было закрывающей скобки
+        throw new Error ('ExpressionError: Brackets must be paired');
+    }
+
+    if (expr.indexOf('/ 0') != -1 || expr.indexOf('/0') != -1) {
+        throw new Error ('TypeError: Division by zero.'); // проверяем, есть ли деление на ноль
+    }
+
+    expr = expr.replace(/ /g, '') && new Function(`return ${expr}`); // убираем пробелы и вызываем функцию, которая считает результат и возвращает его
+    return expr();
 }
 
 module.exports = {
